658f49e93ad908dc5b447031df724deb8da6afd3
Findryankp <findryankurnia@gmail.com>
2023-03-22 15:58:57 +0700

    blob

--- upload.go
+++ upload.go
@@ -3,6 +3,7 @@ package goGcs
 import (
 	"context"
 	"io"
+	"mime/multipart"
 
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/option"
@@ -26,6 +27,34 @@ func UploadImage(bucketName, filePathAndName, gcsFilename string) error {
 	if err1 := LimitMaxSize(file); err1 != nil {
 		return err1
 	}
+
+	//check ext
+	if err1 := IsExtentionImage(filePathAndName); err1 != nil {
+		return err1
+	}
+
+	bucket := client.Bucket(bucketName)
+	obj := bucket.Object(gcsFilename)
+	writer := obj.NewWriter(ctx)
+
+	if _, err := io.Copy(writer, file); err != nil {
+		return err
+	}
+
+	if err := writer.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UploadImageBlob(file multipart.File, bucketName, filePathAndName, gcsFilename string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile(GscCredentialFilename))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
 
 	//check ext
 	if err1 := IsExtentionImage(filePathAndName); err1 != nil {
